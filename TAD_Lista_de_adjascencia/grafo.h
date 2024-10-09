8d275cb09b9c9ceb2839e860e8dc0108f3007870
Math <matheuslearth@gmail.com>
2024-10-09 16:38:02 -0300

    LADJ

--- TAD_Lista_de_adjascencia/grafo.h
+++ TAD_Lista_de_adjascencia/grafo.h
@@ -5,12 +5,12 @@
     #include "item.h"
     #define ERRO -1 // Define -1 como um valor para representar um ERRO.
 
-    // Definição da estrutura de dados GRAFO. Essa estrutura é opaca para o usuario.
-    typedef struct grafo_ GRAFO;
+    // Definição da estrutura de dados LADJ. Essa estrutura é opaca para o usuario.
+    typedef struct listasAdj LADJ;
 
-    GRAFO *grafo_criar(int vertice); // Ao ser chamada essa função retorna um ponteiro que aponta para uma estrutura GRAFO que foi alocada e setada com o número da vertices passado nos parâmetros.
-    bool grafo_insere_aresta(GRAFO *grafo, int cidade_1, int cidade_2, int distancia); // Ao ser chamada a função cria uma ligação não direcionada entre as duas cidades passadas pelos parâmetros e retorna true se conseguiu e false se não.
-    int grafo_get_distancia(GRAFO *grafo, int cidade_1, int cidade_2);// Retorna a distancia entre duas cidades passadas nos parâmetros ou -1 caso não exista conexão entre elas.
-    void grafo_imprimir(GRAFO *grafo); // Imprime todas as listas de adjacência.
-    void grafo_apagar(GRAFO **grafo); // Apaga/desaloca tudo que foi alocado.
+    LADJ *ListaAdj_criar(int vertice); // Ao ser chamada essa função retorna um ponteiro que aponta para uma estrutura LADJ que foi alocada e setada com o número da vertices passado nos parâmetros.
+    bool ListaAdj_insere_caminho(LADJ *listas, int cidade_1, int cidade_2, int distancia); // Ao ser chamada a função cria uma ligação não direcionada entre as duas cidades passadas pelos parâmetros e retorna true se conseguiu e false se não.
+    int ListaAdj_distancia(LADJ *listas, int cidade_1, int cidade_2);// Retorna a distancia entre duas cidades passadas nos parâmetros ou -1 caso não exista conexão entre elas.
+    void ListaAdj_imprimir(LADJ *listas); // Imprime todas as listas de adjacência.
+    void ListaAdj_apagar(LADJ **ListaAdj_distancia); // Apaga/desaloca tudo que foi alocado.
 #endif
